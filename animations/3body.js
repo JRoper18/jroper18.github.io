0cf1417cea52155a3fdf63261cebf42d8ff9a2ab
JRoper18 <jackroper18@gmail.com>
2020-05-17 11:47:25 -0500

    modular-ized animations

--- animations/3body.js
+++ animations/3body.js
@@ -1,10 +1,3 @@
-var element = document.getElementById("launch-background")
-var app = new PIXI.Application({width: element.clientWidth, height: element.clientHeight, antialias: true, transparent: true});
-app.ticker.add(delta => update(delta));
-app.renderer.plugins.interaction.autoPreventDefault = false;
-app.renderer.view.style.touchAction = 'auto';
-
-
 function Point(x, y){
   this.x = x;
   this.y = y;
@@ -22,115 +15,106 @@ function Point(x, y){
     return new Point(this.x * scalar, this.y * scalar);
   }
 }
-var positions = [];
-var velocities = [];
-var masses = [];
-var planets = [];
-var trails = [];
-var colors = [];
-function update(delta){
-  for(var i = 0; i<positions.length; i++){
-    var currentForce = new Point(0, 0);
-    for(var j = 0; j<positions.length; j++){
-      if(i == j){
-        continue;
+
+class ThreeBody extends Animation{
+  constructor(){
+    super();  
+    this.positions = [];
+    this.velocities = [];
+    this.masses = [];
+    this.planets = [];
+    this.trails = [];
+    this.colors = [];
+  }
+  update(delta){
+    for(var i = 0; i<this.positions.length; i++){
+      var currentForce = new Point(0, 0);
+      for(var j = 0; j<this.positions.length; j++){
+        if(i == j){
+          continue;
+        }
+        var dist = this.positions[j].dist(this.positions[i])
+        if(dist <= 1){
+          //They're super close, so they'll probably rocket apart. For the sake of making it look cool, just ignore it this round.
+          continue;
+        }
+        currentForce = currentForce.add(this.positions[j].sub(this.positions[i]).scale(this.masses[j]/(Math.pow(dist, 2))))
       }
-      var dist = positions[j].dist(positions[i])
-      if(dist <= 1){
-        //They're super close, so they'll probably rocket apart. For the sake of making it look cool, just ignore it this round.
-        continue;
+      this.velocities[i] = this.velocities[i].add(currentForce)
+    }
+    for(var i = 0; i<this.velocities.length; i++){
+      var previous = new Point(this.planets[i].x, this.planets[i].y);
+      var deltaPos = this.velocities[i].scale(delta)
+      this.positions[i] = this.positions[i].add(deltaPos)
+      this.planets[i].x = this.positions[i].x + (this.element.clientWidth / 2)
+      this.planets[i].y = this.positions[i].y + (this.element.clientHeight / 2)
+      var line = new PIXI.Graphics();
+      line.lineStyle(4, this.colors[i]/2, 1);
+      line.moveTo(0, 0);
+      line.lineTo(deltaPos.x, deltaPos.y);
+      line.x = previous.x;
+      line.y = previous.y;
+      this.app.stage.addChild(line);
+      this.trails.push(line);
+      if(this.trails.length > 1000){
+        this.app.stage.removeChild(this.trails.shift());
       }
-      currentForce = currentForce.add(positions[j].sub(positions[i]).scale(masses[j]/(Math.pow(dist, 2))))
     }
-    velocities[i] = velocities[i].add(currentForce)
-  }
-  for(var i = 0; i<velocities.length; i++){
-    var previous = new Point(planets[i].x, planets[i].y);
-    var deltaPos = velocities[i].scale(delta)
-    positions[i] = positions[i].add(deltaPos)
-    planets[i].x = positions[i].x + (element.clientWidth / 2)
-    planets[i].y = positions[i].y + (element.clientHeight / 2)
-    var line = new PIXI.Graphics();
-    line.lineStyle(4, colors[i]/2, 1);
-    line.moveTo(0, 0);
-    line.lineTo(deltaPos.x, deltaPos.y);
-    line.x = previous.x;
-    line.y = previous.y;
-    app.stage.addChild(line);
-    trails.push(line);
-    if(trails.length > 1000){
-      app.stage.removeChild(trails.shift());
+    for(var i = 0; i<this.trails.length; i++){
+      this.trails[i].alpha -= 0.003;
     }
+  
   }
-  for(var i = 0; i<trails.length; i++){
-    trails[i].alpha -= 0.003;
+  generatePoints() {
+    var points = []
+    var gAngle = Math.PI * (3 - Math.sqrt(5))
+    var variance = Math.PI / 3
+    var lastAngle = 0
+    var radius = 120;
+    for(var i = 0; i<3; i++){
+      var rand = (Math.random() - 0.5) * 2 //Between -1 and 1
+      lastAngle += (rand * variance) + gAngle
+      points.push(new Point(Math.cos(lastAngle) * radius, Math.sin(lastAngle) * radius))
+    }
+    return points
   }
-
-}
-function generatePoints() {
-  var points = []
-  var gAngle = Math.PI * (3 - Math.sqrt(5))
-  var variance = Math.PI / 3
-  var lastAngle = 0
-  var radius = 120;
-  for(var i = 0; i<3; i++){
-    var rand = (Math.random() - 0.5) * 2 //Between -1 and 1
-    lastAngle += (rand * variance) + gAngle
-    points.push(new Point(Math.cos(lastAngle) * radius, Math.sin(lastAngle) * radius))
+  setup(){
+    this.positions = this.generatePoints();
+    this.velocities = [new Point(0, 0), new Point(0, 0), new Point(0, 0)];
+    this.colors = [0x004400, 0xFF0000, 0x0000FF]
+    this.masses = [3, 4, 5]
+    for(var i = 0; i<this.positions.length; i++){
+      var circle = new PIXI.Graphics();
+      circle.beginFill(this.colors[i]);
+      circle.drawCircle(0, 0, this.masses[i]*2)
+      circle.endFill()
+      this.planets[i] = circle
+      this.app.stage.addChild(circle)
+    }
+    this.app.renderer.autoResize = true;
   }
-  return points
-}
-function setup(){
-  app.stage.removeChildren();
-  positions = generatePoints();
-  velocities = [new Point(0, 0), new Point(0, 0), new Point(0, 0)];
-  colors = [0x004400, 0xFF0000, 0x0000FF]
-  masses = [3, 4, 5]
-  for(var i = 0; i<positions.length; i++){
-    var circle = new PIXI.Graphics();
-    circle.beginFill(colors[i]);
-    circle.drawCircle(0, 0, masses[i]*2)
-    circle.endFill()
-    planets[i] = circle
-    app.stage.addChild(circle)
+  
+  load(element){
+    this.element = element;
+    this.app = new PIXI.Application({width: element.clientWidth, height: element.clientHeight, antialias: true, transparent: true});
+    this.app.renderer.plugins.interaction.autoPreventDefault = false;
+    this.app.renderer.view.style.touchAction = 'auto';
+    element.appendChild(this.app.view)
   }
-  element.appendChild(app.view)
-  app.renderer.autoResize = true;
-}
-
-window.onresize = function(event){
-  for(var i = 0; i<trails.length; i++){ //Clear trails or else they'll look broken and weird.
-    app.stage.removeChild(trails[i])
+  onresize(width, height){
+    for(var i = 0; i<this.trails.length; i++){ //Clear trails or else they'll look broken and weird.
+      app.stage.removeChild(this.trails[i])
+    }
+    app.renderer.resize(width, height)
+  }  
+  mobileReset(){
+    setup();
   }
-  app.renderer.resize(element.clientWidth, element.clientHeight)
-}
-var tapedTwice = false;
-function mobileReset(){
-  if(!tapedTwice) {
-      tapedTwice = true;
-      setTimeout( function() { tapedTwice = false; }, 300 );
-      return false;
+  reset(){
+    app.stage.removeChildren();
+    setup();
   }
-  event.preventDefault();
-  setup();
-}
-$( document ).ready(function() {
-
-  if(window.isMobile){
-    element.addEventListener("click", function(){
-      mobileReset()
-    })
-    element.addEventListener("touchstart", function(){
-      mobileReset()
-    })
-    //Also, change the reset text.
-    document.getElementById("resetText").innerHTML = "Double-tap to reset. "
+  start(){
+    this.app.ticker.add(delta => this.update(delta));
   }
-  else{
-    element.addEventListener("click", function(){
-      setup()
-    })
-  }
-})
-
-setup()
+};
--- animations/conway.js
+++ animations/conway.js
@@ -1,209 +1,196 @@
-var element = document.getElementById("launch-background")
-var app = new PIXI.Application({width: element.clientWidth, height: element.clientHeight, antialias: true, transparent: true});
-app.ticker.add(delta => update(delta));
-app.renderer.plugins.interaction.autoPreventDefault = false;
-app.renderer.view.style.touchAction = 'auto';
-
-var SQUARE_SIZE = 10;
-var BOARD_WIDTH = Math.floor(element.clientWidth / SQUARE_SIZE);
-var BOARD_HEIGHT = Math.floor(element.clientHeight / SQUARE_SIZE);
-var NUM_COLORS = 50;
-
-var colorArray = generateColor("#000000", "#00ffCC", NUM_COLORS);
-
-var totalDelta = 0;
-var TICK_RATE = 5;
-function makeEmptyBoard(){
-  var newBoard = new Array(BOARD_WIDTH);
-  for(var i = 0; i<=BOARD_WIDTH; i++){
-    newBoard[i] = new Array(BOARD_HEIGHT);
-    for(var j = 0; j<BOARD_HEIGHT; j++){
-      newBoard[i][j] = 0;
+class Conway extends Animation{
+  generateColor(colorStart,colorEnd,colorCount){
+  
+    // The beginning of your gradient
+    var start = this.convertToRGB (colorStart);
+  
+    // The end of your gradient
+    var end   = this.convertToRGB (colorEnd);
+  
+    // The number of colors to compute
+    var len = colorCount;
+  
+    //Alpha blending amount
+    var alpha = 0.0;
+  
+    var saida = [];
+  
+    for (var i = 0; i < len; i++) {
+      var c = [];
+      alpha += (1.0/len);
+  
+      c[0] = start[0] * alpha + (1 - alpha) * end[0];
+      c[1] = start[1] * alpha + (1 - alpha) * end[1];
+      c[2] = start[2] * alpha + (1 - alpha) * end[2];
+  
+      saida.push(this.convertToHex (c));
+  
     }
+  
+    return saida;
+  
   }
-  return newBoard;
-}
+ constructor(){
+    super();
 
-function update(delta){
-  totalDelta += delta;
-  if(totalDelta >= TICK_RATE){
-    totalDelta = 0;
-    //Assemble a new board.
-    var newBoard = makeEmptyBoard();
-    for(var i = 0; i<BOARD_WIDTH; i++){
-      for(var j = 0; j<BOARD_HEIGHT; j++){
-        var sCount = 0;
-        if(i > 0 && j > 0 && board[i-1][j-1]){
-          sCount++;
-        }
-        if(i > 0 && j < BOARD_HEIGHT && board[i-1][j+1]){
-          sCount++;
-        }
-        if(i > 0 && board[i-1][j]){
-          sCount++;
-        }
-        if(i < BOARD_WIDTH && j < BOARD_HEIGHT && board[i+1][j+1]){
-          sCount++;
-        }
-        if(i < BOARD_WIDTH && j > 0 && board[i+1][j-1]){
-          sCount++;
-        }
-        if(i < BOARD_WIDTH && board[i+1][j]){
-          sCount++;
-        }
-        if(j < BOARD_HEIGHT && board[i][j+1]){
-          sCount++;
-        }
-        if(j > 0 && board[i][j-1]){
-          sCount++;
-        }
-        if(sCount < 2 || sCount > 3){
-          //Dies.
-          newBoard[i][j] = 0;
-        }
-        else if(board[i][j] > 0) {
-          //Lives on.
-          newBoard[i][j] = board[i][j] + 1;
-        }
-        else if(sCount == 3){
-          //Repopulation!
-          newBoard[i][j] = 1;
-        }
+    this.SQUARE_SIZE = 10;
+    this.NUM_COLORS = 50;
 
+    this.colorArray = this.generateColor("#000000", "#00ffCC", this.NUM_COLORS);
+
+    this.totalDelta = 0;
+    this.TICK_RATE = 5;
+
+  }
+  makeEmptyBoard(){
+    var newBoard = new Array(this.BOARD_WIDTH);
+    for(var i = 0; i<=this.BOARD_WIDTH; i++){
+      newBoard[i] = new Array(this.BOARD_HEIGHT);
+      for(var j = 0; j<this.BOARD_HEIGHT; j++){
+        newBoard[i][j] = 0;
       }
     }
-    board = newBoard;
-    //Now render the space.
-    renderBoard()
+    return newBoard;
   }
-}
-function ageToColor(age){
-  var boundedAge = Math.min(age, NUM_COLORS-1);
-  return parseInt(colorArray[boundedAge], 16);
-}
-function renderBoard() {
-  app.stage.removeChildren()
-  for(var i = 0; i<BOARD_WIDTH; i++){
-    for(var j = 0; j<BOARD_HEIGHT; j++){
-      var liveTime = board[i][j];
-      if(liveTime > 0){
-        var newTile = new PIXI.Graphics();
-        var color = ageToColor(liveTime)
-        newTile.beginFill(color);
-        newTile.drawRect(i * SQUARE_SIZE, j*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-        newTile.endFill();
-        app.stage.addChild(newTile);
+  load(element){
+    var app = new PIXI.Application({width: element.clientWidth, height: element.clientHeight, antialias: true, transparent: true});
+    this.app = app;
+    app.renderer.plugins.interaction.autoPreventDefault = false;
+    app.renderer.view.style.touchAction = 'auto';    
+    this.element = element;
+    this.BOARD_WIDTH = Math.floor(element.clientWidth / this.SQUARE_SIZE);
+    this.BOARD_HEIGHT = Math.floor(element.clientHeight / this.SQUARE_SIZE);
+    app.renderer.autoResize = true;
+    element.appendChild(this.app.view)
+  }  
+  update(delta){
+    this.totalDelta += delta;
+    if(this.totalDelta >= this.TICK_RATE){
+      this.totalDelta = 0;
+      //Assemble a new board.
+      var newBoard = this.makeEmptyBoard();
+      for(var i = 0; i<this.BOARD_WIDTH; i++){
+        for(var j = 0; j<this.BOARD_HEIGHT; j++){
+          var sCount = 0;
+          if(i > 0 && j > 0 && this.board[i-1][j-1]){
+            sCount++;
+          }
+          if(i > 0 && j < this.BOARD_HEIGHT && this.board[i-1][j+1]){
+            sCount++;
+          }
+          if(i > 0 && this.board[i-1][j]){
+            sCount++;
+          }
+          if(i < this.BOARD_WIDTH && j < this.BOARD_HEIGHT && this.board[i+1][j+1]){
+            sCount++;
+          }
+          if(i < this.BOARD_WIDTH && j > 0 && this.board[i+1][j-1]){
+            sCount++;
+          }
+          if(i < this.BOARD_WIDTH && this.board[i+1][j]){
+            sCount++;
+          }
+          if(j < this.BOARD_HEIGHT && this.board[i][j+1]){
+            sCount++;
+          }
+          if(j > 0 && this.board[i][j-1]){
+            sCount++;
+          }
+          if(sCount < 2 || sCount > 3){
+            //Dies.
+            newBoard[i][j] = 0;
+          }
+          else if(this.board[i][j] > 0) {
+            //Lives on.
+            newBoard[i][j] = this.board[i][j] + 1;
+          }
+          else if(sCount == 3){
+            //Repopulation!
+            newBoard[i][j] = 1;
+          }
+  
+        }
+      }
+      this.board = newBoard;
+      //Now render the space.
+      this.renderBoard()
+    }
+  }
+  ageToColor(age){
+    var boundedAge = Math.min(age, this.NUM_COLORS-1);
+    return parseInt(this.colorArray[boundedAge], 16);
+  }
+  renderBoard() {
+    this.app.stage.removeChildren()
+    for(var i = 0; i<this.BOARD_WIDTH; i++){
+      for(var j = 0; j<this.BOARD_HEIGHT; j++){
+        var liveTime = this.board[i][j];
+        if(liveTime > 0){
+          var newTile = new PIXI.Graphics();
+          var color = this.ageToColor(liveTime)
+          newTile.beginFill(color);
+          newTile.drawRect(i * this.SQUARE_SIZE, j*this.SQUARE_SIZE, this.SQUARE_SIZE, this.SQUARE_SIZE);
+          newTile.endFill();
+          this.app.stage.addChild(newTile);
+        }
       }
     }
   }
-}
-function setup(){
-  app.stage.removeChildren();
-  BOARD_WIDTH = Math.floor(element.clientWidth / SQUARE_SIZE);
-  BOARD_HEIGHT = Math.floor(element.clientHeight / SQUARE_SIZE);
+  setup(){
 
-  board = makeEmptyBoard();
-  //Make something random.
-  for(var i = 0; i<BOARD_WIDTH; i++){
-    for(var j = 0; j<BOARD_HEIGHT; j++){
-      var rand = Math.random();
-      if(rand > 0.5){
-        board[i][j] = 1;
+    this.board = this.makeEmptyBoard();
+    //Make something random.
+    for(var i = 0; i<this.BOARD_WIDTH; i++){
+      for(var j = 0; j<this.BOARD_HEIGHT; j++){
+        var rand = Math.random();
+        if(rand > 0.5){
+          this.board[i][j] = 1;
+        }
       }
     }
+    this.renderBoard();
   }
-  renderBoard();
-  element.appendChild(app.view)
-  app.renderer.autoResize = true;
-}
-
-window.onresize = function(event){
-  app.renderer.resize(element.clientWidth, element.clientHeight)
-}
-var tapedTwice = false;
-function mobileReset(){
-  if(!tapedTwice) {
-      tapedTwice = true;
-      setTimeout( function() { tapedTwice = false; }, 300 );
-      return false;
+  reset(){
+    this.app.stage.removeChildren();
+    this.setup();
   }
-  event.preventDefault();
-  setup();
-}
-$( document ).ready(function() {
-  if(window.isMobile){
-    element.addEventListener("click", function(){
-      mobileReset()
-    })
-    element.addEventListener("touchstart", function(){
-      mobileReset()
-    })
-    //Also, change the reset text.
-    document.getElementById("resetText").innerHTML = "Double-tap to reset. "
+  onresize(width, height){
+    app.renderer.resize(width, height);
+    this.BOARD_WIDTH = Math.floor(widht / this.SQUARE_SIZE);
+    this.BOARD_HEIGHT = Math.floor(height / this.SQUARE_SIZE);
   }
-  else{
-    element.addEventListener("click", function(){
-      setup()
-    })
+  mobileReset(){
+    setup();
   }
-})
+  hex (c) {
+    var s = "0123456789abcdef";
+    var i = parseInt (c);
+    if (i == 0 || isNaN (c))
+      return "00";
+    i = Math.round (Math.min (Math.max (0, i), 255));
+    return s.charAt ((i - i % 16) / 16) + s.charAt (i % 16);
+  }
+  
+  /* Convert an RGB triplet to a hex string */
+  convertToHex (rgb) {
+    return this.hex(rgb[0]) + this.hex(rgb[1]) + this.hex(rgb[2]);
+  }
+  
+  /* Remove '#' in color hex string */
+  trim (s) { return (s.charAt(0) == '#') ? s.substring(1, 7) : s }
+  
+  /* Convert a hex string to an RGB triplet */
+  convertToRGB (hex) {
+    var color = [];
+    color[0] = parseInt ((this.trim(hex)).substring (0, 2), 16);
+    color[1] = parseInt ((this.trim(hex)).substring (2, 4), 16);
+    color[2] = parseInt ((this.trim(hex)).substring (4, 6), 16);
+    return color;
+  }
+  start(){
+    this.app.ticker.add(delta => this.update(delta));
+  }
+  
 
-function hex (c) {
-  var s = "0123456789abcdef";
-  var i = parseInt (c);
-  if (i == 0 || isNaN (c))
-    return "00";
-  i = Math.round (Math.min (Math.max (0, i), 255));
-  return s.charAt ((i - i % 16) / 16) + s.charAt (i % 16);
-}
-
-/* Convert an RGB triplet to a hex string */
-function convertToHex (rgb) {
-  return hex(rgb[0]) + hex(rgb[1]) + hex(rgb[2]);
-}
-
-/* Remove '#' in color hex string */
-function trim (s) { return (s.charAt(0) == '#') ? s.substring(1, 7) : s }
-
-/* Convert a hex string to an RGB triplet */
-function convertToRGB (hex) {
-  var color = [];
-  color[0] = parseInt ((trim(hex)).substring (0, 2), 16);
-  color[1] = parseInt ((trim(hex)).substring (2, 4), 16);
-  color[2] = parseInt ((trim(hex)).substring (4, 6), 16);
-  return color;
-}
-
-function generateColor(colorStart,colorEnd,colorCount){
-
-	// The beginning of your gradient
-	var start = convertToRGB (colorStart);
-
-	// The end of your gradient
-	var end   = convertToRGB (colorEnd);
-
-	// The number of colors to compute
-	var len = colorCount;
-
-	//Alpha blending amount
-	var alpha = 0.0;
-
-	var saida = [];
-
-	for (i = 0; i < len; i++) {
-		var c = [];
-		alpha += (1.0/len);
-
-		c[0] = start[0] * alpha + (1 - alpha) * end[0];
-		c[1] = start[1] * alpha + (1 - alpha) * end[1];
-		c[2] = start[2] * alpha + (1 - alpha) * end[2];
-
-		saida.push(convertToHex (c));
-
-	}
-
-	return saida;
-
-}
-
-setup()
+  
+};
