b8347c63a328899dbda0526881e19beb5ea62d75
JRoper18 <jackroper18@gmail.com>
2020-05-17 11:47:42 -0500

    forgot to add

---
+++ animations/animation.js
@@ -0,0 +1,38 @@
+class Animation{
+    constructor(){
+        this.tapedTwice = false;
+    }
+    mobileReset(){
+        throw new Error("You have not implemented a mobile reset method!")
+    }
+    reset(){
+        throw new Error("You have not implemented a reset method!")
+    }
+    load(element){
+        throw new Error("You have not implemented a load method!")
+    }
+    setup(){
+        throw new Error("You have not implemented a setup method!")
+    }
+    update(delta){
+        throw new Error("You have not implemented an update method!")
+    }
+    onresize(width, height){
+        throw new Error("You have not implemented a window resize method!")
+    }
+    destroy(){
+        throw new Error("You have not implemented a destroy method!")
+    }
+    start(){
+        throw new Error("You have not implemented a start method!")
+    }
+    tap(){
+        if(!this.tapedTwice) {
+            this.tapedTwice = true;
+            setTimeout( function() { this.tapedTwice = false; }, 300 );
+            return false;
+        }
+        event.preventDefault();
+        this.mobileReset
+    }
+}
